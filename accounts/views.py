73e1c6e3ae521f559d12a620d7f0465a7cb18ae3
ashiqali <ashiqalit@gmail.com>
2024-01-23 00:52:03 +0530

    Migrated to bootstrap 5.3, Validation added to login using AJAX, User authentication added

--- accounts/views.py
+++ accounts/views.py
@@ -4,12 +4,16 @@ from django.shortcuts import render
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User, auth
 from .forms import CreateUserForm
+from django.http import JsonResponse
 
 
 def registerpage(request):
     form = CreateUserForm()
+    if request.user.is_authenticated:
+        return redirect('home')
     if request.method == 'POST':
         form = CreateUserForm(request.POST)
         if form.is_valid():
@@ -21,21 +25,24 @@ def registerpage(request):
     return render(request, 'store/auth/register.html', context)
 
 def loginpage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
 
         user = authenticate(request, username=username, password=password)
-        if user is not None:
+        if user:
             login(request, user)
-            return redirect('/')
+            return JsonResponse({'success':True})
         else:
-            messages.info(request, 'Username or Password incorrect')
+            # messages.info(request, 'Username or Password incorrect')
+            return JsonResponse({'success':False, 'error':'Invalid Credentials'})
     context={}
     return render(request, 'store/auth/login.html', context)
 
+@login_required(login_url='login')
 def logoutpage(request):
-    
     logout(request)
     messages.success(request, 'Logged out Successfully')
     return redirect('login')
